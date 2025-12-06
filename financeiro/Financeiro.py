ae0bd671b4f4f12b9412325441771af4871fd592
Davi de Oliveira <davi.o.souza6@ba.estudante.senai.br>
2025-12-06 07:12:44 -0300

    1.2

--- financeiro/Financeiro.py
+++ financeiro/Financeiro.py
@@ -26,4 +26,42 @@ def custo_carro(total_despesa, quantidade):
 def calculo_preco(custo_carro):
     return (custo_carro  * 1,5)
 
-
+def gerar_relatorio(despesa, qtd_carro):
+    print("===Relatorio Financeiro===")
+    total = calcular_despesas(despesa)
+    custo_unidade = custo_carro(total, qtd_carro)
+    preco_final = calculo_preco(custo_unidade)
+    
+    print(f"total de despesa: R$ {total:.2f}")
+    print(f"custo por carro: R$ {custo_unidade:.2f}")
+    print(f"preco final de venda: R$ {preco_final:.2f}")
+    
+    
+def menu():
+    despesas = {}
+    while True:
+        print("====Menu Financeiro====")
+        print("1 - cadastrar despesa")
+        print("2 - gerar relatorio")
+        print("3 - sair")
+        opcao = input("Escolha uma opção")
+        
+        if opcao== "1":
+            despesas = cadastrar_despesa()
+            qtd_carros = int(input("Quantidade de carros produzidos por mês: "))
+            print("despesa cadastrada")
+            
+        elif opcao=="2":
+            if not despesas:
+                print("despesa não cadastrada.")
+            else:gerar_relatorio(despesas, qtd_carros)
+                
+        elif opcao=="3":
+            print("sair")
+            break
+        else:
+            print("opção invalida")
+            
+menu()               
+    
+    
