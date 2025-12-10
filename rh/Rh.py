1fb493029437e0209b5c215e85e38a11035b0659
Ezequiel cardoso da Silva <Ezequiel Cardoso da Silva>
2025-12-09 22:39:37 -0300

    carregar, salvar,cadastar,listar,buscar,editar,remover e  folha

--- rh/Rh.py
+++ rh/Rh.py
@@ -1,54 +1,135 @@
-def cadastar_funcionario(lista):
-    
-    print("Cadastro de Funcionário")
-    
-    nome = input("Nome:")
-    cpf = input("CPF")
-    endereco = ("Endereço")
-    telefone = ("Telefone")
-    possui_filhos = ("Possui filhos?")
-    quantidade_de_filhos = ("Quantidade de Filhos")
-    
-    funcionario = {
-        "nome": nome,
-        "cpf": cpf,
-        "endereco": endereco,
-        "telefone": telefone,
-        "possui_filhos": possui_filhos,
-        "quantidade_de_filhos": quantidade_de_filhos
-        }
-    
-    lista.append(funcionario)
+import json
 
-    print("Funcionáio Cadastrado")
+arquivo = "funcionarios.json"
+
+def carregar():
+    try: with open(arquivo,'r',encoding= "utf-8") as f:
+        return json.load(f)
+    except:
+        return[]
     
-def calcular_salario(horas,valor_hora):
-    return horas*valor_hora
+def salvar(lista):  
+    with open(arquivo,"w",enconding = "utf-8") as f:
+        json.dump(lista,f,indent = 2)
+
+def cadastrar():
+    lista = carregar()
+        print("\n=== CADASTRAR FUNCIONÁRIO===")
+        nome = input("nome")
+        cpf = input("CPF:")
+        turno = input("Turno(Manhã/Tarde/Noite):")
+        salario = float(input("Salário"))
+        
+        novo = {
+            "id":len(lista)
+            "nome":nome,
+            "cpf":cpf,
+            "turno":turno,
+            "salario":salario
+        }
+        
+lista.append(novo)
+salvar(lista)
+print("Funcionário cadastrado!\n")
+
+def listar():
+    lista = carregar()
+    print("\n=== LiSTA DE FUNCIONÁRIOS===")
+    if len(lista ==0:)
+    print("Nenhum funcionário cadastrado.")
+    else:
+        print(f"id:{f["id"]} | {f["nome"]}) | CPF:{f["cpf"]} | Turno: {f["turno"]} | {f["salario"]}")
+    print()
     
-def calcular_horas_extra(cargo,horas_extra,valor_hora):
+def buscar ():
+    lista = carregar()
+    nome = input("\n Digite o nome para Buscar: ").lower()
+    achou = false
     
-    if cargo.lower() in ["gerente","diretor"]:
-        return 0
-    return horas_extra* (valor_hora*1.5)
-    
-def calcular_irpf(salario):
-    if salario <= 2000:
-        return 0
-    if salario <= 3500:
-        return salario * 0.75
-    if salario  <= 5000:
-        return salario * 0,15
-    return salario * 0.225
-    
-def salario_liquido(bruto,irpf):
-    return bruto - irpf
-    
-def gerar_relatorios(lista) :
-    print("Relatorio de Funcionários")
-    bruto = f.get("salario_bruto", 0)
-    frpf = calcular_irpf("bruto")
-    liquido = salario = salario_liquido("bruto,irpf")
+    for f in lista:
+        if nome in f["nome"].lower():
+            print(f "ID:{f["id"]} - {f["nome"]} - {f["turno"]} - {f["salario"]}")
+            achou = True 
+        if not achou:
+            print ("Nenhum Funcionário encontrado.\n")
             
-    print(f"\nNome:{f["nome"]}")
-    print(f"Sálario liquido:R$ {liquido:.2f}")
-    print("Paga irpf?","sim" irpf > 0 else "não")
+    print()
+    
+    def editar ():
+        lista = carregar()
+        listar()
+        
+        try:
+            id_escolhido = int(input("ID para editar: "))
+        except:
+            print("ID inválido.\n")
+            return
+        
+        for f in lista:
+            if f["id"] == id_escolhido:
+                print("Deixe Vazio para manter o valor atual.\n")
+            nome = input("Nome novo: ")
+            cpf = input("CPF novo : ")
+            turno = input("Turno novo: ")
+            salario = input("Salário novo: ")   
+        
+        if nome ! = "":
+            f["nome"] = nome
+        if cpf ! = "":
+            f["cpf"] = cpf
+        if turno ! = "":
+            f["turno"] = turno
+        if salario ! = "":
+            f["salario"] = float(salario)
+            
+            salvar(lista)
+            print("Editado com sucesso\n")
+            return
+        print ("ID não encontrado.\n")
+        
+        def remover():
+            lista = carregar()
+            lista()
+            
+            try: 
+                id_escolhido = int(input("ID para remover:))
+            except:
+            print("ID inválido.\n")
+            return
+            
+        nova_lista = []
+        removido = false
+        for f in lista:
+        if f["id"] == id_escolhido:
+         removido = true
+        else nova_lista.append(f)
+         
+        nova_lista.append(f)
+        if removido:
+        salvar(nova_lista)
+        print("Funcionário removido!\n")
+        else: 
+        print("ID não encontrado.")
+        
+        def contar_por_turno():
+        lista = carregar ()
+        cont = {"manha":0,"tarde":0,"noite":0}
+        
+        for f in lista: turno = f ["turno"]
+        if turno in cont:
+        cont[turno] + = 1
+        
+        print("\n===CONTAGEM POR TURNO===")
+        print(cont)
+        print()
+        
+        def folha():
+        lista = carregar ()
+        print("\n===FOLHA SALARIAL===")
+        total = 0
+        
+    for f in lista:
+    print(f"{f["nome"]} - R${f["salario"]}")
+    total + = f["salario"]
+    
+    print("\nTotal da Folha: R$",total,"\n")
